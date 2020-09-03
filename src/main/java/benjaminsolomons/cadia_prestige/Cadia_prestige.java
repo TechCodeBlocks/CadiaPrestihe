796e6f0a13086c4b9b5e5d458ab3cb7659d5ae7c
TechCodeBlocks <benjamin1905.bs@gmail.com>
2020-09-03 19:30:38 +0100

    Initial commit

---
+++ src/main/java/benjaminsolomons/cadia_prestige/Cadia_prestige.java
@@ -0,0 +1,17 @@
+package benjaminsolomons.cadia_prestige;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Cadia_prestige extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
